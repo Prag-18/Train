3a7829c17bca9e2764e51aa0dc2df96f389b2976
Prag-18 <pragatikumari0108@gmail.com>
2026-04-10 09:59:38 +0530

    Pragati added UC3

---
+++ App/src/UC3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class UC3 {
+    public static void main(String[] args){
+        System.out.println("=================================================");
+        System.out.println("    === UC3-Track Unique Bogie IDs ===    ");
+        System.out.println("=================================================");
+        System.out.println("");
+        Set<String> bogies=new HashSet<>();
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
+        System.out.println("");
+        System.out.println("Note:");
+        System.out.println("Duplicates are automatically ignored by HashSet");
+        System.out.println("");
+        System.out.println("UC3 uniqueness validation completed...");
+    }
+}
